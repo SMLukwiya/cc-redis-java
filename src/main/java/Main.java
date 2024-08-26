ebff1c81c30476fe848d06424b91634fb1a1c265
SMLukwiya <sundaymorganl@gmail.com>
2024-08-26 10:15:27 +0300

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -17,8 +19,14 @@ public class Main {
           // ensures that we don't run into 'Address already in use' errors
           serverSocket.setReuseAddress(true);
           // Wait for connection from client.
-          clientSocket = serverSocket.accept();
-          clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
+            clientSocket = serverSocket.accept();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            String input;
+          while ((input = reader.readLine()) != null) {
+              if (input.startsWith("PING")) {
+                  clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
+              }
+          }
         } catch (IOException e) {
           System.out.println("IOException: " + e.getMessage());
         } finally {
